5eb68525d3be27469bcec9b2e018f4f308a340b0
mouradbouajaja <bouajaja.m@gmail.com>
2026-02-27 23:33:18 -0500

    Update site URL to mindsgambit.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [mdx()],
-  site: 'https://mouradbouajaja.netlify.app',
+    integrations: [mdx()],
+    site: 'https://mindsgambit.com',
 });

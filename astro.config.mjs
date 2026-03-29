9d924b024c55e119383747bd9d3f95f31b00d892
mouradbouajaja <bouajaja.m@gmail.com>
2026-03-29 11:26:55 -0400

    Add Vercel adapter and hybrid output configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-    integrations: [mdx(), sitemap()],
-    site: 'https://mindsgambit.com',
+      integrations: [mdx(), sitemap()],
+      site: 'https://mindsgambit.com',
+      output: 'hybrid',
+      adapter: vercel(),
 });
